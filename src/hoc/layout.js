697ef16574bf8bf20908371f58623bd83bd0aa48
Farshid Boroomand <farshidboroomand@gmail.com>
2019-09-07 18:37:30 +0430

    header registered

--- src/hoc/layout.js
+++ src/hoc/layout.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../components/Header/header';
 
 const Layout = (props) => {
   return (
     <div>
+      <Header/>
       { props.children }
     </div>
   );
