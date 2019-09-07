4fb62395c616b10ef6ac76a91762c0f8a5ced6f2
Farshid Boroomand <farshidboroomand@gmail.com>
2019-09-07 19:27:12 +0430

    sidenavItem added

--- src/components/Header/Sidenav/sidenav.js
+++ src/components/Header/Sidenav/sidenav.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import SideNav from 'react-simple-sidenav'
+import SidenavItems from './sidenavItems';
+
 const Nav = (props) => {
   return (
     <SideNav showNav={props.showNav} onHideNav={props.onHideNav} navStyle={{
       background: '#242424',
       maxWidth: '220px'
      }}>
-      Items
+      <SidenavItems/>
     </SideNav>
   );
 };
