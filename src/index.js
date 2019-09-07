62d12986bf548379fc42855384d014a429840024
Farshid Boroomand <farshidboroomand@gmail.com>
2019-09-07 18:08:23 +0430

    configuration completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+import reducers from './reducers';
+import Routes from './routes';
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <Routes/>
+    </BrowserRouter>
+  </Provider>
+  , document.getElementById('root'));
---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+const Routes = () => {
+  return (
+    <div>
+      hiiii
+    </div>
+  );
+};
+
+export default Routes;
